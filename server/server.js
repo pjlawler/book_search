274ecf0b7f447e53f054b3cce0ee1e759ef9310e
Pat Lawler <patlaw777@icloud.com>
2022-05-20 18:47:04 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 
-const app = express();
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth')
+
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +27,29 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
+// express...
+// const path = require('path');
+// const routes = require('./routes');
+// if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+// app.use(routes);
